89b3bc0dc045b4be3239a14077e298690ec93bc1
Petr Yurin <p.yurin@gmail.com>
2019-08-03 22:09:55 +0100

    Tests

--- test/basic_test.py
+++ test/basic_test.py
@@ -1,6 +1,7 @@
 import pyexasol
 import pprint
 import os
+import decimal
 
 host='localhost:8899'
 #host='192.168.1.172:8563'
@@ -8,10 +9,11 @@ host='localhost:8899'
 printer = pprint.PrettyPrinter(indent=4, width=140)
 
 # Basic connect
-C = pyexasol.connect(dsn=host, user='SYS', password='exasol', schema='test')
+C = pyexasol.connect(dsn=host, user='SYS', password='exasol')
 
 # Create schema
 stmt = C.execute("CREATE SCHEMA IF NOT EXISTS test")
+C.open_schema("test")
 
 # Set up go
 stmt = C.execute("ALTER SYSTEM SET SCRIPT_LANGUAGES = 'PYTHON=builtin_python R=builtin_r JAVA=builtin_java GO=localzmq+protobuf:///bfsdefault/default/go/GolangImage?#buckets/bfsdefault/default/go/go_entrypoint/go.sh'");
@@ -49,33 +51,38 @@ if result != [(1, 1, "string1"), (2, 3, "string2"), (3, 6, "string3"), (4, 10, "
 # Different data types test
 
 stmt = C.execute("""
-CREATE OR REPLACE GO SCALAR SCRIPT test.gotest(a DECIMAL(36,0), b DECIMAL(36,36))
-    EMITS (a DECIMAL(36,0), b DECIMAL(36,36)) AS
+CREATE OR REPLACE GO SCALAR SCRIPT test.gotest
+          (a DECIMAL(36,0), b DECIMAL(36,36), c BOOLEAN, d DATE, e TIMESTAMP, f VARCHAR(100), g VARCHAR(2000000))
+    EMITS
+          (a DECIMAL(36,0), b DECIMAL(36,36), c BOOLEAN, d DATE, e TIMESTAMP, f VARCHAR(100), g VARCHAR(2000000))
+AS
 
 package main
 
 import \"exago\"
-import \"github.com/cockroachdb/apd\"
-import \"math/big\"
 
 func Run(iter *exago.ExaIter) {
-    //big int multiplication
-    bigInt := iter.ReadIntBig(0)
-    bigInt2 := big.NewInt(int64(2))
-    bigInt.Mul(bigInt, bigInt2)
-    iter.EmitIntBig(*bigInt)
-
-    //decimal division
-    dec := iter.ReadDecimalApd(1)
-    c:= apd.BaseContext.WithPrecision(36)
-    c.Quo(dec, dec, apd.New(2, 0))
-    iter.EmitDecimalApd(*dec)
+    iter.EmitIntBig(*iter.ReadIntBig(0))
+    iter.EmitDecimalApd(*iter.ReadDecimalApd(1))
+    iter.EmitBool(*iter.ReadBool(2))
+    iter.EmitDate(*iter.ReadDate(3))
+    iter.EmitTime(*iter.ReadTime(4))
+    iter.EmitString(*iter.ReadString(5))
+    iter.EmitString(*iter.ReadString(6))
 }
 /
 """);
 
-result = C.execute("SELECT test.gotest(400000000140000000014000000001123456, 0.800000004800000004800000004222468, True, '9999-12-31', '9999-12-31 23:59:59.999')").fetchall()
-if result != [('800000000280000000028000000002246912', '0.400000002400000002400000002111234')]:
+result = C.execute("SELECT test.gotest("
+                   + ('9' * 36) + ", "
+                   + '0.' + ('9' * 36) + ", "
+                   + "True, "
+                   + "'9999-12-31', "
+                   + "'9999-12-31 23:59:59.999', "
+                   + "'" + ('ひ' * 100) + "'" + ", "
+                   + "'" + ('ひ' * 2000000) + "'"
+                   + ")").fetchall()
+if result != [('9' * 36, '0.' + ('9' * 36), "True", "9999-12-31", "9999-12-31 23:59:59.999", ('ひ' * 100), ('ひ' * 2000000))]:
     raise Exception("Different data types test failed, result set\n", result)
 
 
