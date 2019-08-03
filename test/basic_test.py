e43b95d2ba2c68712afb4733ec371a767ec19669
Petr Yurin <p.yurin@gmail.com>
2019-08-03 20:03:07 +0100

    Tests

--- test/basic_test.py
+++ test/basic_test.py
@@ -2,19 +2,19 @@ import pyexasol
 import pprint
 import os
 
-dsn = os.environ.get('EXAHOST', 'localhost:8563')
-user = os.environ.get('EXAUID', 'SYS')
-password = os.environ.get('EXAPWD', 'exasol')
-schema = os.environ.get('EXASCHEMA', 'test')
-
 printer = pprint.PrettyPrinter(indent=4, width=140)
 
 # Basic connect
-C = pyexasol.connect(dsn=dsn, user=user, password=password, schema=schema)
+C = pyexasol.connect(dsn='localhost:8563', user='SYS', password='exasol', schema='test')
 
 # Create schema
 stmt = C.execute("CREATE SCHEMA IF NOT EXISTS test")
 printer.pprint(stmt.fetchall())
+
+# Basic query
+stmt = C.execute("SELECT now()")
+printer.pprint(stmt.fetchall())
+
 
 # Basic query
 stmt = C.execute("SELECT * FROM users ORDER BY user_id LIMIT 5")
