9f1c0c21dca608180e6691a809c354099497db0d
Petr Yurin <p.yurin@gmail.com>
2019-08-11 16:25:26 +0100

    Install scripts refactoring; external libs uploading feature

--- test/test_libs.py
+++ test/test_libs.py
@@ -1,82 +1,61 @@
 import pyexasol
 import pprint
 import sys
+import os
 import _test_lib
 
 C = _test_lib.init_exasol_test()
 
-# SET SCRIPT RETURNS, SCALAR SCRIPT EMITS are tested in other tests.
-# Let's also double check that SCALAR SCRIPT RETURNS and SET SCRIPT EMITS work
+if len(sys.argv) < 4:
+    raise Exception("Usage: " + sys.argv[0] + " EXASOL_DB_HOST_PORT EXASOL_BUCKET_HOST_PORT EXASOL_BUCKETFS_USERPASS")
 
-# SCALAR SCRIPT RETURNS
+bucketHostPort = sys.argv[2]
+bucketUserPass = sys.argv[3]
 
-stmt = C.execute("""
-CREATE OR REPLACE GO SCALAR SCRIPT test.gotest
-          (a DECIMAL(16,0))
-    RETURNS
-          DECIMAL(16,0)
-AS
+# Using local lib ./test/testnumbers
+r = os.system("./upload_lib_local.sh " + bucketHostPort + " 'default/go' " + "'" + bucketUserPass + "' " + os.path.dirname(os.path.realpath(__file__)) + "/testnumbers")
+if r != 0:
+    raise Exception("Failed running upload_lib_local.sh")
 
-package main
-
-import \"exago\"
-
-func Run(iter *exago.ExaIter) *int64 {
-    return iter.ReadInt64(0)
-}
-/
-""");
-
-result = C.execute("SELECT test.gotest(v) FROM (SELECT 16 as v UNION ALL SELECT 17 UNION ALL SELECT 21)").fetchall()
-if result != [(16,),(17,),(21,)]:
-    raise Exception("'Scalar script returns' test failed, result set\n", result)
-print("'Scalar script returns' - ok")
-
-# SET SCRIPT EMITS
-
-stmt = C.execute("""
-CREATE OR REPLACE GO SET SCRIPT test.gotest
-          (a DECIMAL(16,0), b BOOLEAN)
-    EMITS
-          (a DECIMAL(16,0), b VARCHAR(50))
-AS
+C.execute("""
+CREATE OR REPLACE GO SCALAR SCRIPT test.gotest(a DECIMAL(16,0)) RETURNS BOOLEAN AS
 
 package main
 
-import \"exago\"
+import "exago"
+import "testnumbers"
 
-func Run(iter *exago.ExaIter) {
-    for true {
-        if *iter.ReadBool(1) == true {
-            var i int64;
-            for i = *iter.ReadInt64(0); i > 2; i = i / 2 {
-                iter.EmitInt64(i)
-                iter.EmitString("Cycle in progress")
-            }
-            iter.EmitInt64(i)
-            iter.EmitString("Cycle finished")
-        }
-        if !iter.Next() {
-            break;
-        }
-    }
+func Run(iter *exago.ExaIter) *bool {
+    b := testnumbers.IsOdd(*iter.ReadInt64(0))
+    return &b;
 }
-/
-""");
+""")
+result = C.execute("SELECT test.gotest(2015)").fetchall()
+if result != [(True,)]:
+    raise Exception("Local lib test failed, result set\n", result)
+print("Local lib test - ok")
 
-result = C.execute("SELECT test.gotest(a, b) FROM ("
-                   "SELECT 17 as a, false as b "
-                   "UNION ALL "
-                   "SELECT 63 as a, false as b "
-                   "UNION ALL "
-                   "SELECT 17 as a, false as b "
-                   "UNION ALL "
-                   "SELECT 4 as a, true as b "
-                   "UNION ALL "
-                   "SELECT 16 as a, true as b "
-                   "UNION ALL "
-                   "SELECT 96 as a, false as b "
-                   ")").fetchall()
-if result != [(4,"Cycle in progress"),(2,"Cycle finished"),(16,"Cycle in progress"),(8,"Cycle in progress"),(4,"Cycle in progress"),(2,"Cycle finished")]:
-    raise Exception("'Set script emits' test failed, result set\n", result)
-print("'Set script emits' - ok")
+
+
+# Using github lib https://github.com/visualfc/fibutil
+r = os.system("./upload_lib_github.sh " + bucketHostPort + " 'default/go' " + "'" + bucketUserPass + "' " + "https://github.com/visualfc/fibutil")
+if r != 0:
+    raise Exception("Failed running upload_lib_github.sh")
+
+C.execute("""
+CREATE OR REPLACE GO SCALAR SCRIPT test.gotest(a DECIMAL(16,0)) RETURNS DECIMAL(36, 0) AS
+
+package main
+
+import "exago"
+import "math/big"
+import "github.com/visualfc/fibutil/fib"
+
+func Run(iter *exago.ExaIter) *big.Int {
+    return fib.Fib(*iter.ReadInt64(0));
+}
+""")
+result = C.execute("SELECT test.gotest(10)").fetchall()
+if result != [('55',)]:
+    raise Exception("Github lib test failed, result set\n", result)
+print("Github lib test - ok")
